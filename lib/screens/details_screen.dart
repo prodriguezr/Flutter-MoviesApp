ee00ef33cefdec56192451edbe8674b6fba8cdc8
Pablo Rodríguez <prodrigu75@gmail.com>
2021-07-17 13:51:33 -0400

    Overview and actor's cards added

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,11 +3,179 @@ import 'package:flutter/material.dart';
 class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final String _movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no movie';
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Details Screen'),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            _PosterAndTitle(),
+            _Overview(),
+            _Overview(),
+            _Overview(),
+            _CastingCards()
+          ])),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: EdgeInsets.all(0),
+        centerTitle: true,
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          padding: EdgeInsets.only(bottom: 15),
+          child: Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
         ),
+        background: FadeInImage(
+            placeholder: AssetImage('assets/loading.gif'),
+            image: NetworkImage('https://via.placeholder.com/500x300'),
+            fit: BoxFit.cover),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(
+        top: 20,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 200,
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: theme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originalTitle',
+                style: theme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 1,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star_outline,
+                    size: 25,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(
+                    width: 7,
+                  ),
+                  Text(
+                    'movie.voteAverage',
+                    style: theme.caption,
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _Overview extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+      child: Text(
+        'Ex Lorem et eiusmod officia Lorem consequat Lorem. In ad pariatur do voluptate aute qui occaecat veniam ex aliqua culpa consectetur eu fugiat. Nostrud consectetur laboris quis adipisicing tempor aliquip ut minim aliqua non sit irure. Nostrud Lorem proident dolore ea excepteur nostrud nisi. Ex laborum adipisicing officia anim mollit sunt enim nulla in dolore amet nisi sit.',
+        style: Theme.of(context).textTheme.subtitle1,
+        textAlign: TextAlign.justify,
+      ),
+    );
+  }
+}
+
+class _CastingCards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 30),
+      width: double.infinity,
+      height: 180,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, int index) => _CastCard(),
+        itemCount: 10,
+      ),
+    );
+  }
+}
+
+class _CastCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      width: 110,
+      height: 300,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/150x300'),
+              height: 140,
+              width: 100,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'actor.name fegewgewewgewg',
+            style: TextStyle(),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+          )
+        ],
       ),
     );
   }
